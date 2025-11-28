2a23a7fa75dfefed6f50515688ac120b2d560aaa
Vertifelis <green.cat.hell@gmail.com>
2025-11-28 10:59:30 +0500

    refactor: replace internal dependency with constructor injection

--- src/services/google-spreadsheets.service.ts
+++ src/services/google-spreadsheets.service.ts
@@ -1,17 +1,7 @@
 import { sheets_v4 } from "@googleapis/sheets";
-import { GoogleAuth } from "google-auth-library";
 
 export class GoogleSpreadsheetsService {
-    private sheets: sheets_v4.Sheets;
-
-    constructor() {
-        const auth = new GoogleAuth({
-            keyFile: "google_key.json",
-            scopes: "https://www.googleapis.com/auth/spreadsheets",
-        });
-
-        this.sheets = new sheets_v4.Sheets({ auth });
-    }
+    constructor(private sheets: sheets_v4.Sheets) {}
 
     public async syncTariffSpreadsheets(spreadsheetIds: string[], values: unknown[][], range: string) {
         const jobs = spreadsheetIds.map((spreadsheetId) => this.updateSpreadsheet(spreadsheetId, values, range));
